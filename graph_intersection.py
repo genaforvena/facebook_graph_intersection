d3a8cdd9e990988a8882c8fa7aa14687dd187a86
imozerov <ilyailiilia@gmail.com>
2014-04-17 20:17:49 +0400

    Added Facebook authentifacator class

--- graph_intersection.py
+++ graph_intersection.py
@@ -1,8 +1,12 @@
 import argparse
 import csv
+import cookielib
+import mechanize #pip install mechanize
 import requests # pip install requests
+import sys
 import urllib
 
+from requests.packages.urllib3 import add_stderr_logger
 from bs4 import BeautifulSoup
 
 
@@ -17,9 +21,11 @@ def get_command_line_options():
         find mutual friends between given persons on Facebook.
 
         example usage:
-        python graph_intersection.py input_csv_file_path''')
+        python graph_intersection.py input_csv_file_path email@gmail.com password''')
 
     argparser.add_argument('input')
+    argparser.add_argument('login')
+    argparser.add_argument('password')
 
     args = argparser.parse_args()
     return args
@@ -29,7 +35,8 @@ class IntersectionSearcher(object):
     ID = "id"
     EMAIL = "email"
 
-    def __init__(self):
+    def __init__(self, browser):
+        self.browser = browser
         self._output_file = DEFAULT_OUTPUT
         self._base_emails_and_ids = []
         self._target_emails_and_ids = []
@@ -66,9 +73,9 @@ class IntersectionSearcher(object):
                     writer.writerow([x.encode("utf-8") for x in result_row_list])
 
     def _get_person_id(self, email):
-        response = requests.get("https://www.facebook.com/search.php?o=2048&init=dir&q=" + urllib.quote_plus(email))
-        data = response.text
-        soup = BeautifulSoup(data)
+        request = "https://www.facebook.com/search.php?q=" + urllib.quote_plus(email)
+        response = self.browser.get(request)
+        soup = BeautifulSoup(response)
         try:
             person_name_div = soup.find_all("div", attrs={'class': 'instant_search_title'})[0].find("a")
             onclick_text = person_name_div.attrs["onclick"]
@@ -84,6 +91,54 @@ class IntersectionSearcher(object):
         return intersections_list
 
 
+class FacebookAuthBrowser(object):
+    def __init__(self, login, password):
+        self._login = login
+        self._password = password
+        self._login_url = 'https://facebook.com/login'
+        # self.session = None
+        self._browser = None
+
+    def login(self):
+        self._browser = mechanize.Browser()
+        cj = cookielib.LWPCookieJar()
+        self._browser.set_cookiejar(cj)
+
+        self._browser.set_handle_equiv(True)
+        self._browser.set_handle_redirect(True)
+        self._browser.set_handle_referer(True)
+        self._browser.set_handle_robots(False)
+        self._browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        self._browser.addheaders = [('User-agent', 'Chrome')]
+
+        self._browser.open(self._login_url)
+        self._browser.select_form(nr=0)
+
+        self._browser.form['email'] = self._login
+        self._browser.form['pass'] = self._password
+
+        self._browser.submit()
+
+
+
+        # self.session = requests.Session()
+        # self.session.headers['User-Agent'] = 'Chrome'
+        #
+        # login = {'login': self.login, 'password': self.password}
+        # login_response = self.session.post(self.url, data=login)
+        # for r in login_response.history:
+        #     if r.status_code == 401:  # 401 means authentication failed
+        #         print 'error!'
+        #         sys.exit(1)  # abort
+
+    def get(self, request):
+        return self._browser.open(request).read()
+
+
+class PersonIdNotFoundException(Exception):
+    pass
+
+
 def _get_list(json_data):
     return_list = []
     for x in json_data:
@@ -92,13 +147,11 @@ def _get_list(json_data):
     return return_list
 
 
-class PersonIdNotFoundException(Exception):
-    pass
-
-
 if __name__ == "__main__":
     args = get_command_line_options()
-    searcher = IntersectionSearcher()
+    browser = FacebookAuthBrowser(args.login, args.password)
+    browser.login()
+    searcher = IntersectionSearcher(browser)
     searcher.read_input_file(args.input)
     searcher.find_intersections_and_print()
 
